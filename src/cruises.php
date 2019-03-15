905c5f584860a45e7c80efe683f914e821b0f84f
Juan Garcia <jrjgg@hotmail.com>
2019-03-15 09:55:00 -0400

    developing views

--- src/cruises.php
+++ src/cruises.php
@@ -28,30 +28,18 @@ include('includes/head.php');
   <div class="content">
     <?php if ($lang=='en'){ ?>
       <h1>Alaska Cruises</h1>
-    <?php } else { ?>
-      <h1>Kreuzfahrten</h1>
-    <?php } ?>
-
-    <?php if ($lang=='en'){ ?>
       <p>Many Cruise Companies travel along the Inside Passage. Most of them with big ships with 2000 or more pax capacities. I only work with small ship Cruise Companies, from 10 to not even 90 persons on board. It ensures you, that you can visit places that the big guys just can’t get to. Small fjords and the option to get on land and explore the shoreline on foot or with a kayak sounds more exciting to me, than ships with a casino, gym etc. The smaller Cruise Companies have a slower pace, so you will have more time to explore and enjoy nature.</p>
       <p>The smaller Cruise Ships book out quicker of course, as availability is a lot more limited.</p>
       <h3>Pre- & Post Cruise Trip planning</h3>
       <p>We can help you arrange a custom trip before or after your Cruise. Most of the visitors would like to spend some time in Denali Park. Please be aware, that the Denali Park Road does not open till about the first week in June and will close around mid September. Regardless or the road opening you have a chance to see Mt. Denali (former Mt McKinley) if the weather conditions are right, even from Anchorage!</p>
-
     <?php } else { ?>
+      <h1>Kreuzfahrten</h1>
       <p>Viele Kreuzfahrtunternehmen reisen entlang der Inside Passage. Diese Kreuzfahrtschiffe sind riesig und bieten Platz für mehr als 2000 Passagiere. Wir arbeiten nur mit kleinen Schiffunternehmen zusammen, wessen Schiffe maximal 90 Personen befördern können.</p>
       <p>Das erlaubt Ihnen, an Orte zu gelangen, an welche die grossen Schiffe gar nicht hinkommen. Der Besuch kleiner Fjorde und die Möglichkeit, an Land gehen zu können, zu Fuss oder mit einem Kajak, finden wir spannender als Boardcasinos oder Fitnessstudios. Die kleineren Schiffunternehmen schlagen ein langsameres Tempo an, was Ihnen mehr Zeit bietet, die Landschaft zu erkunden und die Natur zu geniessen.</p>
       <p>Je kleiner die Schiffe, desto schneller sind sie ausgebucht. Verfügbare Plätze auf den kleinen Schiffen sind dementsprechend rarer!</p>
       <h3>Zur Organisation Ihrer Reise vor und nach Ihrer Kreuzfahrt</h3>
       <p>Wir können Ihnen bei der Planung und Buchung ihrer Alaskareise rund um Ihre gewählte Kreuzfahrt helfen. Die meisten Bucher möchten den Denali Nationalpark besuchen. Die Strasse zum Denali Nationalpark ist jedoch nur von Anfang Juni bis Mitte September offen und befahrbar. Unabhängig davon haben Sie aber gute Chancen den Berg Denali (ehemals Mt. McKinley) zu sehen, da er bei gutem Wetter auch von Anchorage aus zu sehen ist.</p>
     <?php } ?>
-    <!--
-    <div class="thumbs">
-      <figure><a data-fancybox="home-fsh" class="thumbs-34" href="assets/images/home-fishing-0.jpg"><img src="assets/images/home-fishing-0.jpg" width="300"></a></figure>
-      <figure><a data-fancybox="home-fsh" class="thumbs-34" href="assets/images/home-fishing-2.jpg"><img src="assets/images/home-fishing-2.jpg" width="300"></a></figure>
-      <figure><a data-fancybox="home-fsh" class="thumbs-34" href="assets/images/home-fishing-1.jpg"><img src="assets/images/home-fishing-1.jpg" width="300"></a></figure>
-    </div>
-    -->
   </div>
 </article>
 
@@ -64,6 +52,38 @@ include('includes/head.php');
       <h2>The Boat Company</h2>
       <p>Das Unternehmen betreibt von Mitte Mai bis September zwei Kleinschiffe mit maximal 22 respektive 24 Plätzen, die zwischen Juneau und Sitka verkehren. Nebst grossartigem Service auf den Schiffen haben Sie die Möglichkeit, auf der Reise auch zu kajaken, zu wandern und/oder zu fischen. Alle Schiffskabinen haben ein eigenes Bad mit Dusche.</p>
     <?php } ?>
+    <div class="gallery">
+      <figure>
+        <a data-fancybox="tbc" href="assets/images/cruises/the-boat-company-0.jpg">
+          <img src="assets/images/cruises/the-boat-company-0-thumb@2x.jpg" width="150">
+        </a>
+      </figure>
+      <figure>
+        <a data-fancybox="tbc" href="assets/images/cruises/the-boat-company-1.jpg">
+          <img src="assets/images/cruises/the-boat-company-1-thumb@2x.jpg" width="150">
+        </a>
+      </figure>
+      <figure>
+        <a data-fancybox="tbc" href="assets/images/cruises/the-boat-company-2.jpg">
+          <img src="assets/images/cruises/the-boat-company-2-thumb@2x.jpg" width="150">
+        </a>
+      </figure>
+      <figure>
+        <a data-fancybox="tbc" href="assets/images/cruises/the-boat-company-3.jpg">
+          <img src="assets/images/cruises/the-boat-company-3-thumb@2x.jpg" width="150">
+        </a>
+      </figure>
+      <figure>
+        <a data-fancybox="tbc" href="assets/images/cruises/the-boat-company-4.jpg">
+          <img src="assets/images/cruises/the-boat-company-4-thumb@2x.jpg" width="150">
+        </a>
+      </figure>
+      <figure>
+        <a data-fancybox="tbc" href="assets/images/cruises/the-boat-company-5.jpg">
+          <img src="assets/images/cruises/the-boat-company-5-thumb@2x.jpg" width="150">
+        </a>
+      </figure>
+    </div>
   </div>
 </article>
 
